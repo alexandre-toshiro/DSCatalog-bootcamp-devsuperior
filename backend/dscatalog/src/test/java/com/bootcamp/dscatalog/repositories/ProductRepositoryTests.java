a98ec5adb13e57f9a6e92195f835c901c4975365
alexandre-toshiro <atoshiro@indracompany.com>
2021-10-20 18:57:59 -0300

    Before Each

--- backend/dscatalog/src/test/java/com/bootcamp/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/dscatalog/src/test/java/com/bootcamp/dscatalog/repositories/ProductRepositoryTests.java
@@ -1,31 +1,50 @@
 package com.bootcamp.dscatalog.repositories;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.bootcamp.dscatalog.entities.Product;
 
 @DataJpaTest
 public class ProductRepositoryTests {
-	
+
 	@Autowired
 	private ProductRepository repository;
 	
+	private long existingId;
+	private long nonExistingId;
+	
+	@BeforeEach// Usado para instânciar uma variável antes de todos os testes
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		// A vantagem desse método é que caso essas váriaveis se repitam em diversos testes, não será necessário a declaração
+		// em cada um deles.
+	}
+
 	@Test
 	public void deleteShouldDeleteObjectWhenIdExists() {
-		long existingId = 1;
-		
+
 		repository.deleteById(existingId);
 		Optional<Product> result = repository.findById(existingId);
-		
+
 		assertFalse(result.isPresent());
 	}
-	
-	
+
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExists() {
+
+		assertThrows(EmptyResultDataAccessException.class, () -> {
+			repository.deleteById(nonExistingId);
+		});
+	}
 
 }
