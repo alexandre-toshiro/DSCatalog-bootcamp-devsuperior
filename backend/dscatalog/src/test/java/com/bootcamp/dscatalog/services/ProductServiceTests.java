74fa60a1e58e4c2c5b5dacaee9ee74b18a66b832
alexandre-toshiro <atoshiro@indracompany.com>
2021-10-20 21:50:18 -0300

    Simulating various behaviors and testing findAllPaged

--- backend/dscatalog/src/test/java/com/bootcamp/dscatalog/services/ProductServiceTests.java
+++ backend/dscatalog/src/test/java/com/bootcamp/dscatalog/services/ProductServiceTests.java
@@ -1,21 +1,33 @@
 package com.bootcamp.dscatalog.services;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.bootcamp.dscatalog.dto.ProductDTO;
+import com.bootcamp.dscatalog.entities.Product;
 import com.bootcamp.dscatalog.repositories.ProductRepository;
 import com.bootcamp.dscatalog.services.exceptions.DatabaseException;
 import com.bootcamp.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.bootcamp.dscatalog.tests.Factory;
 
 // Não carrega o contexto, mas permite usar os recursos do Spring com Junit(testes de unidade)
 @ExtendWith(SpringExtension.class)
@@ -27,6 +39,9 @@ public class ProductServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	
+	private PageImpl<Product> page;// Tipo concreto do Pageable.
+	private Product product;
 
 	@Mock
 	private ProductRepository repository;
@@ -36,6 +51,8 @@ public class ProductServiceTests {
 		existingId = 1L;
 		nonExistingId = 1000L;
 		dependentId = 4L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 
 		Mockito.doNothing().when(repository).deleteById(existingId);
 		// Quando mocamos um objeto, devemos fazer um simulação do comportamento
@@ -43,6 +60,22 @@ public class ProductServiceTests {
 
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(dependentId);
+		// Métodos void, primeiro temos ação e depois o when.
+		
+		// Métodos com retorno, primeiro usamos o when e depois a ação.
+		
+	    Mockito.when(repository.findAll((Pageable) ArgumentMatchers.any())).thenReturn(page);
+	    //ArgumArgumentMatchers - Simulação, n importa muito o tipo, podendo ser "qualquer coisa"
+	    //Fizemos o cast, por conta da sobrecarga do findAll que espera tipos específicos.
+	    
+	    //Simulando comportamento do save
+	    Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
+	    
+	    //Simulando comportamento do findByid com Id EXISTENTE.
+	    Mockito.when(repository.findById(existingId)).thenReturn(Optional.of(product));// Instância optional com produto.
+	    
+	    //Simulando comportamento do findByid com Id INEXISTENTE.
+	    Mockito.when(repository.findById(nonExistingId)).thenReturn(Optional.empty());
 
 	}
 
@@ -73,6 +106,15 @@ public class ProductServiceTests {
 		});
 
 		Mockito.verify(repository, Mockito.times(1)).deleteById(dependentId);
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageable);
+		
+		assertNotNull(result);
+		Mockito.verify(repository).findAll(pageable);
 	}
 
 }
