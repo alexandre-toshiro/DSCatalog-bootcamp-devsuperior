1f9818623236d5db512ca16c5df5fc6f76512834
alexandre-toshiro <atoshiro@indracompany.com>
2021-10-21 21:02:03 -0300

    Testing findById - Controller

--- backend/dscatalog/src/test/java/com/bootcamp/dscatalog/controllers/ProductControllerTests.java
+++ backend/dscatalog/src/test/java/com/bootcamp/dscatalog/controllers/ProductControllerTests.java
@@ -3,6 +3,7 @@ package com.bootcamp.dscatalog.controllers;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import com.bootcamp.dscatalog.dto.ProductDTO;
 import com.bootcamp.dscatalog.services.ProductService;
+import com.bootcamp.dscatalog.services.exceptions.ResourceNotFoundException;
 import com.bootcamp.dscatalog.tests.Factory;
 
 @WebMvcTest(ProductController.class)
@@ -34,6 +36,9 @@ public class ProductControllerTests {
 	private PageImpl<ProductDTO> page;
 	// PageImpl - Página concreta
 
+	private long existingId = 1L;
+	private long nonExistingId = 2L;
+
 	@BeforeEach
 	void setUp() throws Exception {
 
@@ -42,14 +47,37 @@ public class ProductControllerTests {
 
 		when(service.findAllPaged(any())).thenReturn(page);
 
+		when(service.findById(existingId)).thenReturn(productDTO);
+		when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+
 	}
 
 	@Test
 	public void findAllShouldReturnPage() throws Exception {
 		// perform - faz uma requisição.
-		ResultActions result = mockMvc.perform(get("/products")
+		ResultActions result = mockMvc.perform(get("/products").accept(MediaType.APPLICATION_JSON));
+		result.andExpect(status().isOk());
+	}
+
+	@Test
+	public void findByIdShouldReturnProductDTOWhenIdExists() throws Exception {
+		ResultActions result = mockMvc.perform(get("/products/{id}", existingId)
 				.accept(MediaType.APPLICATION_JSON));
 		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.id").exists());// verificar se existe aquele atributo no objeto da resposta.
+		result.andExpect(jsonPath("$.name").exists());
+		result.andExpect(jsonPath("$.description").exists());
+		// $ - Acessa o objeto da resposta(análisa o json
+		// Podemos análisar apenas alguns atributos ou todos.
+	}
+
+	@Test
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExists() throws Exception {
+		ResultActions result = mockMvc.perform(get("/products/{id}", nonExistingId)
+				.accept(MediaType.APPLICATION_JSON));
+		result.andExpect(status().isNotFound());// 404
+		// Como na camada de controller a ResourceNotFound foi tratada pelo controllerAdvice
+		// Não irá retornar a exceção, sim o status 404 - notFound
 	}
 
 }
