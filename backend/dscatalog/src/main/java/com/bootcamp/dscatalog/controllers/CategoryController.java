8a500680dfb427718601d7e19f68d4c614098c6d
alexandre-toshiro <atoshiro@indracompany.com>
2021-10-31 15:41:40 -0300

    Method name correction

--- backend/dscatalog/src/main/java/com/bootcamp/dscatalog/controllers/CategoryController.java
+++ backend/dscatalog/src/main/java/com/bootcamp/dscatalog/controllers/CategoryController.java
@@ -56,7 +56,7 @@ public class CategoryController {
 	}
 	
 	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@PathVariable Long id) {
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		
 		return ResponseEntity.noContent().build();// corpo da resposta vazio na exclusão.
