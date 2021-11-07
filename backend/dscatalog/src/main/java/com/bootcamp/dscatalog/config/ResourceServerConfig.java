a1eee96ab2900c0e15995497268fdbf175a0e17c
alexandre-toshiro <atoshiro@indracompany.com>
2021-11-06 21:36:16 -0300

    H2-console security configuration

--- backend/dscatalog/src/main/java/com/bootcamp/dscatalog/config/ResourceServerConfig.java
+++ backend/dscatalog/src/main/java/com/bootcamp/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.bootcamp.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -12,14 +15,17 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
-	
+
+	@Autowired
+	private Environment env;// ambiente da aplicação com spring
+
 	@Autowired
 	private JwtTokenStore tokenStore;
-	
-	private static final String[] PUBLIC = {"/oauth/token"};
-	private static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
-	
-	private static final String[] ADMIN = {"/users/**"};
+
+	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+
+	private static final String[] ADMIN = { "/users/**" };
 
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -31,11 +37,16 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		//Aqui configuramos as rotas.
-		http.authorizeRequests()
-		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()// libera AS ROTAS na função GET(todo mundo pode ver os produtos)
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
-		.antMatchers(ADMIN).hasRole("ADMIN")
-		.anyRequest().authenticated();		
+		
+		// h2-console
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+		
+		http.authorizeRequests().antMatchers(PUBLIC).permitAll().antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN)
+				.permitAll()// libera AS ROTAS na função GET(todo mundo pode ver os produtos)
+				.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN")
+				.antMatchers(ADMIN).hasRole("ADMIN")
+				.anyRequest().authenticated();
 	}
 }
